ac4d23b605f39c318ff638ce3d5bba7a64fd216e
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-20 14:56:29 +0200

    Fix terminal (#188)

--- application/src/main/java/com/codbex/iapetus/IapetusApplication.java
+++ application/src/main/java/com/codbex/iapetus/IapetusApplication.java
@@ -28,8 +28,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class IapetusApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(IapetusApplication.class, args);
+    private static long startedAt;
+
+    public static long getStartedAt() {
+        return startedAt;
     }
 
+    public static void main(String[] args) {
+        startedAt = System.currentTimeMillis();
+        System.out.println("------- Application is starting -------");
+        SpringApplication.run(IapetusApplication.class, args);
+    }
 }
