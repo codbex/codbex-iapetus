119bb1cd0b5aa3a7551970deb0b7aa6b59c1dba2
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 15:43:53 +0200

    fixes

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusMessagingFacadeIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.iapetus.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.api.java.messaging.MessagingFacadeIT;
-
-class IapetusMessagingFacadeIT extends MessagingFacadeIT {
-}
--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusODataAPIIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.iapetus.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.api.rest.ODataAPIIT;
-
-class IapetusODataAPIIT extends ODataAPIIT {
-}
