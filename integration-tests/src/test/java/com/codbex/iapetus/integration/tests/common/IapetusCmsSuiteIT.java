1ffa31e2a581fb8007a836b639221bc5846af1bd
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 12:00:16 +0200

    cleanup

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusCmsSuiteIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.iapetus.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.api.javascript.cms.CmsSuiteIT;
-
-class IapetusCmsSuiteIT extends CmsSuiteIT {
-}
