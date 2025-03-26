658a3f051a75b5dc00604643a93653c8a4a01f9a
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:05:10 +0200

    Add more integration tests from dirigible (#196)

---
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusMailIT.java
@@ -0,0 +1,6 @@
+package com.codbex.iapetus.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.ui.tests.MailIT;
+
+class IapetusMailIT extends MailIT {
+}
---
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusSecurityIT.java
@@ -0,0 +1,6 @@
+package com.codbex.iapetus.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.api.SecurityIT;
+
+class IapetusSecurityIT extends SecurityIT {
+}
