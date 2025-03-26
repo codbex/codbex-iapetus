20ada1414e34f7f4469ac3f43de8cb74f06817d0
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:35:49 +0200

    add GitPerspectiveIT (#197)

---
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/common/IapetusGitPerspectiveIT.java
@@ -0,0 +1,6 @@
+package com.codbex.iapetus.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.ui.tests.GitPerspectiveIT;
+
+class IapetusGitPerspectiveIT extends GitPerspectiveIT {
+}
