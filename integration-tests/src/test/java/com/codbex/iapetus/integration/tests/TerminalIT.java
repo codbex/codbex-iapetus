fc35697d0d09835f84afff101e78689f44336221
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-06-05 15:47:58 +0300

    rerun

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
@@ -22,8 +22,7 @@ public class TerminalIT extends IapetusIntegrationTest {
                                                                .addArguments("--disable-gpu")
                                                                .addArguments("--no-sandbox")
                                                                .addArguments("--disable-dev-shm-usage");
-        //        Configuration.browserSize = "1920x1080";
-        Configuration.browserSize = null;
+        Configuration.browserSize = "1920x1080";
 
         Workbench workbench = ide.openWorkbench();
 
