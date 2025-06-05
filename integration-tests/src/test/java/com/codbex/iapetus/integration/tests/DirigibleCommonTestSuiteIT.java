f8d3db781a5828cd99f2304cc65f8fd7bf0d966f
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-06-05 14:30:04 +0300

    new cfg

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/DirigibleCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/DirigibleCommonTestSuiteIT.java
@@ -46,9 +46,11 @@ public class DirigibleCommonTestSuiteIT {
     static {
         // TODO: to be removed once https://github.com/eclipse-dirigible/dirigible/pull/5104
         // is released
-        Configuration.browserSize = "1920x1080";
+        //        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = null;
 
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
 
         Configuration.browserCapabilities = options;
