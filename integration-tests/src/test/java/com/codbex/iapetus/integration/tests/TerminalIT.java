d2253e9c54a96c72bf770c4bba8103a1e751b6ec
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-06-05 16:15:48 +0300

    rerun with Xvfb

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
@@ -3,7 +3,6 @@ package com.codbex.iapetus.integration.tests;
 import com.codeborne.selenide.Configuration;
 import org.eclipse.dirigible.tests.framework.ide.Workbench;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 //@Import(TerminalTestRestConfigDiffName.class)
@@ -15,14 +14,8 @@ public class TerminalIT extends IapetusIntegrationTest {
     @Test
     void testTerminalWorks() {
         Configuration.browser = "chrome";
-        Configuration.browserSize = null;
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // or just --headless
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
+        Configuration.headless = false;
+        Configuration.browserSize = "1920x1080"; // now this will work
 
         Workbench workbench = ide.openWorkbench();
 
