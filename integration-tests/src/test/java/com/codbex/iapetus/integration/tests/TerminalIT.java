208a4b79c293dc3cb9ea19db135919a1f4049db4
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-06-06 09:53:10 +0300

    rerun

--- integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
+++ integration-tests/src/test/java/com/codbex/iapetus/integration/tests/TerminalIT.java
@@ -1,6 +1,5 @@
 package com.codbex.iapetus.integration.tests;
 
-import com.codeborne.selenide.Configuration;
 import org.eclipse.dirigible.tests.framework.ide.Workbench;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -13,10 +12,6 @@ public class TerminalIT extends IapetusIntegrationTest {
 
     @Test
     void testTerminalWorks() {
-        Configuration.browser = "chrome";
-        Configuration.headless = false;
-        Configuration.browserSize = "1920x1080"; // now this will work
-
         Workbench workbench = ide.openWorkbench();
         workbench.openTerminal();
     }
